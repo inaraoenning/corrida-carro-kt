b91414b33b01202bda07a77d21c9a1c20b2304d5
Inara Oenning <inaraoenning@gmail.com>
2026-03-30 17:45:16 -0300

    Add classe pista e funcao main

---
+++ src/Pista.kt
@@ -0,0 +1,32 @@
+class Pista(
+    val listaDeCarros: List<Carro>,
+    val voltas: Int,
+    val clima: String
+) {
+    // Inicia com uma lista vazia que será preenchida depois
+    var posicoes: List<Carro> = listOf()
+
+
+    //Função que simula a corrida
+
+    //Pontuação de desempenho = atributos do carro + as habilidades dos pilotos,
+    // quem tiver a maior pontuação final ganha a corrida
+
+    fun simularCorrida(){
+        // Ordena os carros do maior desempenho para o menor
+        posicoes = listaDeCarros.sortedByDescending { carro->
+            carro.velocidade + carro.aceleracao + carro.piloto.habilidade
+        }
+    }
+
+    // Exibir placar final
+    fun mostrarPlacar(){
+        println("--- 🏁 PLACAR FINAL 🏁 ---")
+        // forEachIndexed passa por cada carro e nos da a posição (index) dele
+        posicoes.forEachIndexed { index, carro ->
+            val desempenho = carro.velocidade + carro.aceleracao + carro.piloto.habilidade
+            println("${index + 1}º Lugar: ${carro.piloto.nome} (Carro com desempenho: $desempenho)")
+        }
+     }
+
+}
--- src/main.kt
+++ src/main.kt
@@ -9,7 +9,7 @@ fun main() {
 
     // Instância dos 10 carros e pilotos
     val piloto1 = Piloto("Ayrton", 43, 10)
-    val piloto2= Piloto("Relampago Marquinhos", 10, 10)
+    val piloto2 = Piloto("Relampago Marquinhos", 10, 10)
     val piloto3 = Piloto("Max Verstappen", 26, 98)
     val piloto4 = Piloto("Lewis Hamilton", 39, 97)
     val piloto5 = Piloto("Charles Leclerc", 26, 95)
@@ -19,8 +19,8 @@ fun main() {
     val piloto9 = Piloto("George Russell", 26, 91)
     val piloto10 = Piloto("Sergio Perez", 34, 88)
 
-    val carro1 = Carro (200, 15, 10, 50, 5, 100, piloto1)
-    val carro2= Carro (300, 25, 10, 40, 8, 40, piloto2)
+    val carro1 = Carro(200, 15, 10, 50, 5, 100, piloto1)
+    val carro2 = Carro(300, 25, 10, 40, 8, 40, piloto2)
     val carro3 = Carro(320, 95, 90, 100, 8, 85, piloto3)
     val carro4 = Carro(318, 93, 92, 100, 7, 88, piloto4)
     val carro5 = Carro(322, 96, 88, 100, 9, 82, piloto5)
@@ -29,4 +29,16 @@ fun main() {
     val carro8 = Carro(310, 91, 87, 100, 8, 84, piloto8)
     val carro9 = Carro(316, 92, 91, 100, 8, 87, piloto9)
     val carro10 = Carro(308, 85, 86, 100, 7, 90, piloto10)
+
+    // Função listOf para Agrupar elementos (Lista de carros)
+    val listaDeLargada = listOf(carro1, carro2, carro3, carro4, carro5, carro6, carro7, carro8, carro9, carro10)
+
+    // Criando a pista
+    val monaco = Pista(listaDeLargada, 50, "Ensolarado")
+
+    // Simulando a corrida e mostrando o resultado
+    println("A corrida de ${monaco.voltas} voltar começou! Clima: ${monaco.clima}")
+    monaco.simularCorrida()
+    monaco.mostrarPlacar()
+
 }
